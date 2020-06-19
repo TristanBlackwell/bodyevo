4a57933042a7de9bafe779d107e04a8cac23c3d0
Tristan Blackwell <tristanblackwell1@gmail.com>
2020-06-19 19:09:34 +0100

    change react router redirects

---
+++ client/build/precache-manifest.c3ad58d0dee445294d56fe1949a7b255.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01efb3469900b235612050d5643d18e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e8bcb915d13f1e9da41",
+    "url": "/static/css/main.fe6baaef.chunk.css"
+  },
+  {
+    "revision": "ef00c4122f3e6f813efa",
+    "url": "/static/js/2.400c7177.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.400c7177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e8bcb915d13f1e9da41",
+    "url": "/static/js/main.3edbaf38.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "f3fcaba76edb49fdf1f62b1712b2cb76",
+    "url": "/static/media/beautyHero.f3fcaba7.jpg"
+  },
+  {
+    "revision": "b8b67c7777f68ff9179949d0ca6b0e95",
+    "url": "/static/media/classes.b8b67c77.jpg"
+  },
+  {
+    "revision": "ac127bf0b601636e4fed4336ff0a3ca6",
+    "url": "/static/media/evoLogo.ac127bf0.png"
+  },
+  {
+    "revision": "a013034d6e7a4b95eb4be8391eeeae52",
+    "url": "/static/media/evoMassage.a013034d.png"
+  },
+  {
+    "revision": "b74e9867da9d2ceec743421cd6251ed3",
+    "url": "/static/media/groupworkout.b74e9867.jpg"
+  },
+  {
+    "revision": "3bd6b509506ee17e4c34245ce9acb7d8",
+    "url": "/static/media/gymHero.3bd6b509.jpg"
+  },
+  {
+    "revision": "bc3a28c139923192eac40bbd9637a9fe",
+    "url": "/static/media/gymPic1.bc3a28c1.jpg"
+  },
+  {
+    "revision": "6f35664943cd1f1c2c7460deb13bece4",
+    "url": "/static/media/gymPic3.6f356649.jpg"
+  },
+  {
+    "revision": "681c9589d2fc04c56a5d15cc30ce278f",
+    "url": "/static/media/hero.681c9589.jpg"
+  },
+  {
+    "revision": "75bfb8db93be60f637414cda5919a0fa",
+    "url": "/static/media/massaging.75bfb8db.jpg"
+  },
+  {
+    "revision": "e7574ef32f58abc6e8eb86e2712232d7",
+    "url": "/static/media/signpost.e7574ef3.jpg"
+  },
+  {
+    "revision": "d393f85b4a51f3f5b69e8c7132c13645",
+    "url": "/static/media/treadmillResize.d393f85b.jpg"
+  }
+]);
