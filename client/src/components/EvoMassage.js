bc54ffcfc5e7d04db8cda85270857a62c0f05640
Tristan Blackwell <tristanblackwell1@gmail.com>
2020-12-22 10:12:56 +0000

    Revert "Add CMS implementation to various components"
    This reverts commit aa3d6534ba36473745b04b013c210fff04792de2.

--- client/src/components/EvoMassage.js
+++ client/src/components/EvoMassage.js
@@ -3,182 +3,205 @@ import { NavLink } from "react-router-dom";
 
 import evoMassageLogo from "../images/evoMassage.png";
 
-const query = `
-{
-  covidNoticeCollection {
-    items {
-      body
-      openDate
-      openTimes
-    }
-  }
-  evoTreatmentCollection {
-    items {
-      title
-      description
-      pricing
-    }
-  }
-}
-`
-
 class EvoMassage extends Component {
-
-  constructor() {
-    super();
-
-    this.state = {
-      response: null,
-      data: null,
-      notice: null,
-      treatments: null,
-      loaded: false
-    }
-  }
-
-  componentDidMount() {
-    fetch(process.env.REACT_APP_CONTENTFUL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        authorization: "Bearer " + process.env.REACT_APP_TOKEN,
-      },
-      body: JSON.stringify({query})
-    })
-    .then(res => res.json())
-    .then(res => {
-      if (res.errors) {
-        this.setState({
-          response: "Error",
-          loaded: true
-        })
-      } else {
-        var treatments = Object.keys(res.data.evoTreatmentCollection.items).map((key) => [res.data.evoTreatmentCollection.items[key]]);
-        this.setState({
-          response: "Success",
-          data: res.data.evoTreatmentCollection,
-          notice: res.data.covidNoticeCollection,
-          treatments: treatments,
-          loaded: true
-        })
-      }
-    })
-  }
-
   render() {
-
-    if (this.state.loaded) {
-      return (
-        <div>
-          <div id="beautyHero" className="gymHeader">
-            <h2 className="center">Evo massage</h2>
-            <p className="center">Massage therapy for greater wellbeing</p>
-          </div>
-          <div className="container covidAlert">
-            <div className="col s12 m6 l6 center">
-              <h2 className="header">Covid-19 Notice</h2>
-              <div className="card horizontal">
-                <div className="card-stacked">
-                  <div className="card-content">
-                    <p style={{whiteSpace: "pre-wrap"}}>
-                      <b>
-                        {this.state.notice.items[0].body}
-                      </b>
-                    </p>
-                    <b></b>
-                  </div>
-                  <div className="card-action" id="waitingList">
-                    <NavLink to="/contact">
-                      Please call or email to book an appointment
-                    </NavLink>
-                  </div>
+    return (
+      <div>
+        <div id="beautyHero" className="gymHeader">
+          <h2 className="center">Evo massage</h2>
+          <p className="center">Massage therapy for greater wellbeing</p>
+        </div>
+        <div className="container covidAlert">
+          <div className="col s12 m6 l6 center">
+            <h2 className="header">Covid-19 Notice</h2>
+            <div className="card horizontal">
+              <div className="card-stacked">
+                <div className="card-content">
+                  <p>
+                    <b>
+                      Evo Massage is now open once again folllowing the latest
+                      government guidance. Treatments are as always designed
+                      with the greatest care and cleanliness in mind for you to
+                      safely recieve your next massage to the highest standard.
+                    </b>
+                  </p>
+                  <b></b>
+                </div>
+                <div className="card-action" id="waitingList">
+                  <NavLink to="/contact">
+                    Please call or email to book an appointment
+                  </NavLink>
                 </div>
               </div>
             </div>
           </div>
-          <div className="container" id="srBio">
-            <div className="row">
-              <div className="col center s12 m12 l6">
-                <img src={evoMassageLogo} alt="Sharon" id="bioImage" />
+        </div>
+        <div className="container" id="srBio">
+          <div className="row">
+            <div className="col center s12 m12 l6">
+              <img src={evoMassageLogo} alt="Sharon" id="bioImage" />
+            </div>
+            <div className="col s12 m12 l6">
+              <h3 className="center">Meet Sharon</h3>
+              <h5 className="center" id="srProfession">
+                Level 5 Sports Massage Therapist
+              </h5>
+              <p>
+                Hello, my name is Sharon. Alongside running Body evo with my
+                great team I offer massage and rehab therapy through Evo
+                Massage.
+                <br />
+                <br />
+                My aim is to help fellow ladies in not only getting fit through
+                our classes and gym but with avoiding all the aches and tension
+                that can be built up daily through the busy lives we all lead.
+                Having massage therapy is great to supplement your training
+                regime and we have many different services on offer to suit
+                particular requirements or needs.
+                <br />
+                Please see below some of what we offer and if theres anything
+                you may be unsure of or have a question to ask feel free to get
+                in touch or find me at the gym for a chat!
+              </p>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div className="divider" />
+        <h3 className="center" id="sliderTitle">
+          Some of our treatments
+        </h3>
+        <p id="evoDesc">
+          <b>
+            We aim to cater to everyones needs whatever they may be. This list
+            is not everything we have to offer but just our key and most
+            commonly requested treatments. Being a Body evo member is not
+            required in order to use Evo massage and should you wish to book a
+            session or ask a question just pop in or give us a call on
+            07985310161. We look forward to seeing you!
+          </b>
+        </p>
+        <div className="container treatments">
+          <div className="row">
+            <div className="col s12 m6 l4">
+              <div className="card">
+                <div className="card-content">
+                  <span className="card-title">Relaxation</span>
+                  <p>
+                    A comforting long stroke massage aimed at generally removing
+                    any aches and helping promote blood flow around the body.
+                  </p>
+                </div>
+                <div className="card-action">
+                  <a href="#!">30 minutes</a>
+                  <a href="#!">£24.50</a>
+                </div>
+                <div className="card-action">
+                  <a href="#!">1 hour</a>
+                  <a href="#!">£45</a>
+                </div>
               </div>
-              <div className="col s12 m12 l6">
-                <h3 className="center">Meet Sharon</h3>
-                <h5 className="center" id="srProfession">
-                  Level 5 Sports Massage Therapist
-                </h5>
-                <p>
-                  Hello, my name is Sharon. Alongside running Body evo with my
-                  great team I offer massage and rehab therapy through Evo
-                  Massage.
-                  <br />
-                  <br />
-                  My aim is to help fellow ladies in not only getting fit through
-                  our classes and gym but with avoiding all the aches and tension
-                  that can be built up daily through the busy lives we all lead.
-                  Having massage therapy is great to supplement your training
-                  regime and we have many different services on offer to suit
-                  particular requirements or needs.
-                  <br />
-                  Please see below some of what we offer and if theres anything
-                  you may be unsure of or have a question to ask feel free to get
-                  in touch or find me at the gym for a chat!
-                </p>
+            </div>
+            <div className="col s12 m6 l4">
+              <div className="card">
+                <div className="card-content">
+                  <span className="card-title">Deep Tissue Massage</span>
+                  <p>
+                    higher pressure massage targetting the deeper layers of
+                    muscles and tissue. Helps aid recovery while also removing
+                    nagging pains or tightness.
+                  </p>
+                </div>
+                <div className="card-action">
+                  <a href="#!">30 minutes</a>
+                  <a href="#!">£23.50</a>
+                </div>
+                <div className="card-action">
+                  <a href="# ">1 hour</a>
+                  <a href="# ">£45</a>
+                </div>
+              </div>
+            </div>
+            <div className="col s12 m6 l4">
+              <div className="card">
+                <div className="card-content">
+                  <span className="card-title">Sports Injuries</span>
+                  <p>
+                    For those sufferring from previous injuries causing
+                    difficulty in day to day life or training. This massage is
+                    for relieving such struggles and returning to training.
+                  </p>
+                </div>
+                <div className="card-action">
+                  <a href="#!">Duration varies</a>
+                  <a href="#!">Price varies</a>
+                </div>
+              </div>
+            </div>
+            <div className="col s12 m6 l4">
+              <div className="card">
+                <div className="card-content">
+                  <span className="card-title">Rehab</span>
+                  <p>
+                    Site-specific treatment with an aim for your ideal goal or
+                    outcome. develop a personal plan for rehabbing and getting
+                    back on track as soon as possible.
+                  </p>
+                </div>
+                <div className="card-action">
+                  <a href="#!">Duration varies</a>
+                  <a href="#!">Price varies</a>
+                </div>
+              </div>
+            </div>
+            <div className="col s12 m6 l4">
+              <div className="card">
+                <div className="card-content">
+                  <span className="card-title">Neck &amp; Shoulders</span>
+                  <p>
+                    Suffering from headaches, neck pain or general tension? This
+                    shorter, more focused massage is designed to combat daily
+                    accumulated stress at its origin.
+                  </p>
+                </div>
+                <div className="card-action">
+                  <a href="#!">Duration varies</a>
+                  <a href="#!">£12.50 - £25</a>
+                </div>
+              </div>
+            </div>
+            <div className="col s12 m6 l4">
+              <div className="card">
+                <div className="card-content">
+                  <span className="card-title">Sports Massage</span>
+                  <p>
+                    Targetted soft tissue massage. Assists in correcting
+                    imbalances from repetitive movements, preventing injury and
+                    increasing flexibility
+                  </p>
+                </div>
+                <div className="card-action">
+                  <a href="#!">
+                    30 Minutes
+                    <br /> (1 area)
+                  </a>
+                  <a href="#!">£25.50</a>
+                </div>
+                <div className="card-action">
+                  <a href="#!">1 hour (2+ areas)</a>
+                  <a href="#!">£40</a>
+                </div>
               </div>
             </div>
           </div>
-          <br />
-          <div className="divider" />
-          <h3 className="center" id="sliderTitle">
-            Some of our treatments
-          </h3>
-          <p id="evoDesc">
-            <b>
-              We aim to cater to everyones needs whatever they may be. This list
-              is not everything we have to offer but just our key and most
-              commonly requested treatments. Being a Body evo member is not
-              required in order to use Evo massage and should you wish to book a
-              session or ask a question just pop in or give us a call on
-              07985310161. We look forward to seeing you!
-            </b>
-          </p>
-          <div className="container treatments">
-            <div className="row">
-              {this.state.treatments.map(treatment => {
-                return (
-                  <div key={treatment[0].title} className="col s12 m6 l4">
-                    <div className="card">
-                      <div className="card-content">
-                        <span className="card-title">{treatment[0].title}</span>
-                        <p>{treatment[0].description}</p>
-                      </div>
-                        {Object.entries(treatment[0].pricing).map(([key, value]) => {
-                          return (
-                            <div key={key}className="card-action">
-                              <a href="#!">{key}</a>
-                              <a href="#!">{value}</a>
-                            </div>
-                          )
-                        })}
-                    </div>
-                  </div>
-                )
-              })}
-            </div>
+        </div>
+        <footer>
+          <div className="footer-copyright valign-wrapper">
+            <div className="container">© 2020 Copyright Bodyevo</div>
           </div>
-          <footer>
-            <div className="footer-copyright valign-wrapper">
-              <div className="container">© 2020 Copyright Bodyevo</div>
-            </div>
-          </footer>
-        </div>
-      );
-    } else {
-      return (
-        <div id="loading">
-          <h1>Loading...</h1>
-        </div>
-      )
-    }
+        </footer>
+      </div>
+    );
   }
 }
 
